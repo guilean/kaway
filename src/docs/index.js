e539d7572a92f92340f71a8e798f40ec1b5b09ed
Pau Teruel <93.pau.teruel@gmail.com>
2020-04-02 22:03:52 +0200

    feat(Button): initial setup

---
+++ src/docs/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Button } from "../lib/Button";
+
+const App = () => (
+  <div>
+    <h1>Demo</h1>
+    <h2>Button</h2>
+    <Button text="Click me!" />
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById("root"));
