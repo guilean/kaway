ab0edc55a961fe9b7409c0c493dce95590c83507
Ivan <ivan.lopez.medina.ilm@gmail.com>
2020-04-04 20:26:19 +0200

    feat: add button props

--- src/lib/Button/Button.js
+++ src/lib/Button/Button.js
@@ -2,15 +2,42 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Button.scss';
 
-const Button = ({ text, disabled }) => (
-	<button className="button" disabled={disabled}>
-		{text}
-	</button>
-);
+const Button = ({
+	children,
+	disabled,
+	startIcon,
+	endIcon,
+	fullWidth,
+	href,
+	size,
+}) => {
+	return (
+		<button className="button" disabled={disabled}>
+			{startIcon && startIcon}
+			{children}
+			{endIcon && endIcon}
+		</button>
+	);
+};
 
 Button.propTypes = {
-	text: PropTypes.string,
+	children: PropTypes.node.isRequired,
 	disabled: PropTypes.bool,
+	startIcon: PropTypes.node,
+	endIcon: PropTypes.node,
+	fullWidth: PropTypes.bool,
+	href: PropTypes.string,
+	size: PropTypes.string,
+};
+
+Button.defaultProps = {
+	children: null,
+	disabled: true,
+	startIcon: undefined,
+	endIcon: undefined,
+	fullWidth: false,
+	href: undefined,
+	size: 'medium',
 };
 
 export default Button;
