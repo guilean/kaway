3c5241c38e831e13611bf4c4aa720c0c492123e2
Pau Teruel <93.pau.teruel@gmail.com>
2020-04-05 11:40:40 +0200

    test(Button): add more coverage for Button

--- src/lib/Button/Button.test.js
+++ src/lib/Button/Button.test.js
@@ -8,7 +8,7 @@ describe('<Button/>', () => {
 		cleanup();
 	});
 
-	it('snnapshot', () => {
+	it('snapshot', () => {
 		const { getByRole } = render(<Button children={'Dummy'} />);
 		const component = getByRole('button');
 		expect(component).toMatchSnapshot();
@@ -20,22 +20,66 @@ describe('<Button/>', () => {
 		expect(component.nodeName).toBe('BUTTON');
 	});
 
-	it('should have default btn class when rendered', () => {
+	it('should have default class when render', () => {
 		const { getByRole } = render(<Button children={'Dummy'} />);
 		const component = getByRole('button');
 		expect(component).toHaveClass('btn');
 	});
 
-	it('should change class with size prop', () => {
+	it('should have modifier class when size prop', () => {
 		const { getByRole } = render(<Button children={'Dummy'} size="large" />);
 		const component = getByRole('button');
 		expect(component).toHaveClass('btn--large');
 	});
 
-	it('should have disabled attribute', () => {
+	it('should have modifier class when fullWidth prop', () => {
+		const { getByRole } = render(<Button children={'Dummy'} fullWidth />);
+		const component = getByRole('button');
+		expect(component).toHaveClass('btn--fullWidth');
+	});
+
+	it('should have disabled attribute when disabled prop', () => {
 		const { getByRole } = render(<Button children={'Dummy'} disabled />);
 		const component = getByRole('button');
 		expect(component.hasAttribute('disabled')).toBeTruthy();
+	});
+
+	it('should render startIcon when startIcon prop', () => {
+		const { getByRole } = render(
+			<Button children={'Dummy'} startIcon={<i>startIcon</i>} />
+		);
+		const component = getByRole('button');
+		expect(component.firstChild.nodeName).toBe('I');
+	});
+
+	it('should render endIcon when endIcon prop', () => {
+		const { getByRole } = render(
+			<Button children={'Dummy'} endIcon={<i>endIcon</i>} />
+		);
+		const component = getByRole('button');
+		expect(component.lastChild.nodeName).toBe('I');
+	});
+
+	it('should render startIcon and endIcon when startIcon and endIcon prop', () => {
+		const { getByRole } = render(
+			<Button
+				children={'Dummy'}
+				startIcon={<i>startIcon</i>}
+				endIcon={<i>endIcon</i>}
+			/>
+		);
+		const component = getByRole('button');
+		expect(component.firstChild.nodeName).toBe('I');
+		expect(component.lastChild.nodeName).toBe('I');
+	});
+
+	it('should not render startIcon when startIcon prop is null', () => {
+		const { getByRole } = render(
+			<Button children={'Dummy'} startIcon={null} />
+		);
+		const component = getByRole('button');
+		expect(component.firstChild.nodeName).not.toBe('I');
+		expect(component.nodeName).toBe('BUTTON');
 	});
 
 	it('should call onClick when button is clicked', () => {
