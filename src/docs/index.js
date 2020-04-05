2beed2d986d256b1da1a7f778d52dae2eb3bda32
Pau Teruel <93.pau.teruel@gmail.com>
2020-04-05 12:45:07 +0200

    docs: apply kaway

--- src/docs/index.js
+++ src/docs/index.js
@@ -3,11 +3,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => (
-	<div>
-		<h1>Demo</h1>
-		<h2>Button</h2>
+	<main>
+		<div align="center">
+			<img
+				width="200"
+				src="https://images.vexels.com/media/users/3/154568/isolated/preview/9a39776ad90adf8a623a9dab8047fc39-emoticon-feliz-kawaii-by-vexels.png"
+				alt="Kawaui logo"
+			/>
+		</div>
+		<h1 style={{ fontFamily: 'Roboto' }} align="center">
+			Kaway Components
+		</h1>
 		<Button children="ADD NEW COMPONENT" />
-	</div>
+	</main>
 );
 
 ReactDOM.render(<App />, document.getElementById('root'));
