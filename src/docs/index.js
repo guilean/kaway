01f7f7f5ea69764d8a14da2b147c1fa7cef31f26
Pau Teruel <93.pau.teruel@gmail.com>
2020-04-05 12:33:24 +0200

    style(Button): apply basic style to component

--- src/docs/index.js
+++ src/docs/index.js
@@ -1,12 +1,12 @@
+import { Button } from '../lib/Button';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Button } from '../lib/Button';
 
 const App = () => (
 	<div>
 		<h1>Demo</h1>
 		<h2>Button</h2>
-		<Button text="Click me!" />
+		<Button children="ADD NEW COMPONENT" />
 	</div>
 );
 
