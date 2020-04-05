ffc86b86750280f74dd8a5bedfba55da9d2b98bc
Pau Teruel <93.pau.teruel@gmail.com>
2020-04-05 11:16:34 +0200

    test(Button): add test coverage for button component

--- src/lib/Button/Button.test.js
+++ src/lib/Button/Button.test.js
@@ -1,3 +1,50 @@
-test('adds 1 + 2 to equal 3', () => {
-	expect(1 + 2).toBe(3);
+import { cleanup, fireEvent, render } from '@testing-library/react';
+
+import Button from './Button';
+import React from 'react';
+
+describe('<Button/>', () => {
+	afterEach(() => {
+		cleanup();
+	});
+
+	it('snnapshot', () => {
+		const { getByRole } = render(<Button children={'Dummy'} />);
+		const component = getByRole('button');
+		expect(component).toMatchSnapshot();
+	});
+
+	it('should render a button node element', () => {
+		const { getByRole } = render(<Button children={'Dummy'} />);
+		const component = getByRole('button');
+		expect(component.nodeName).toBe('BUTTON');
+	});
+
+	it('should have default btn class when rendered', () => {
+		const { getByRole } = render(<Button children={'Dummy'} />);
+		const component = getByRole('button');
+		expect(component).toHaveClass('btn');
+	});
+
+	it('should change class with size prop', () => {
+		const { getByRole } = render(<Button children={'Dummy'} size="large" />);
+		const component = getByRole('button');
+		expect(component).toHaveClass('btn--large');
+	});
+
+	it('should have disabled attribute', () => {
+		const { getByRole } = render(<Button children={'Dummy'} disabled />);
+		const component = getByRole('button');
+		expect(component.hasAttribute('disabled')).toBeTruthy();
+	});
+
+	it('should call onClick when button is clicked', () => {
+		const mockFn = jest.fn();
+		const { getByRole } = render(
+			<Button onClick={mockFn} children={'Dummy'} />
+		);
+		const component = getByRole('button');
+		fireEvent.click(component);
+		expect(mockFn).toHaveBeenCalledTimes(1);
+	});
 });
